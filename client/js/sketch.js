cb25fa99986c7eac23809642fa574b831ac71838
DUROXOID <105690710+DUROXOID@users.noreply.github.com>
2023-09-14 18:52:18 -0700

    textured
    yes

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -15,6 +15,7 @@ var mouseStopDist = 0.8;
 var shellBox, shellDome, shellSpike;
 var turtle, turtleHead, turtleJaw, turtleFoot, turtleTail;
 var stem, leaf, flowerWhite, flowerYellow;
+var HideUI, BoxRollUI, DomeRollUI, SpikeRollUI;
 
 var mapSize = 6000;
 
@@ -22,7 +23,7 @@ var mapSize = 6000;
 function preload(){
 	shellBox = loadImage('assets/shellBox.png');
 	shellDome = loadImage('assets/shellDome.png');
-	// shellSpike = loadImage('assets/shellSpike.png');
+  	shellSpike = loadImage('assets/shellSpike.png');
 	// turtle = loadImage('assets/turtle.png');
 	turtleHead = loadImage('assets/turtleHead.png');
 	// turtleJaw = loadImage('assets/turtle.png');
@@ -31,7 +32,11 @@ function preload(){
 	stem = loadImage('assets/stem.png');
 	leaf = loadImage('assets/leaf.png');
 	flowerWhite = loadImage('assets/flowerWhite.png');
-	flowerYellow = loadImage('assets/flowerYellow.png');
+
+	HideUI = loadImage('assets/turtleHead.png');
+	BoxRollUI = loadImage('assets/shellBox.png');
+	DomeRollUI = loadImage('assets/shellDome.png');
+	SpikeRollUI = loadImage('assets/shellSpike.png');
 }
 
 //first thing that is called. Sets up everything
@@ -43,7 +48,7 @@ function setup() {
 
     socket = io();
     
-    socket.emit("imReady", {name: "Devlogerio"});
+    socket.emit("imReady", {name: "Boxt.io Player"}); //not actually used I dont think
 
     socket.on("yourId", function(data) {
         myId = data.id;
@@ -57,7 +62,7 @@ function setup() {
     });
 
     socket.on("initPack", function(data) {
-        for(var i in data.initPack) {
+        for(let i in data.initPack) {
             var player = new Player(data.initPack[i].id, data.initPack[i].name, data.initPack[i].x, data.initPack[i].y, data.initPack[i].size);
             players.push(player);
             console.log("player init with: "+myId);
@@ -65,12 +70,13 @@ function setup() {
     });
 
     socket.on("updatePack", function(data) {
-        for(var i in data.updatePack) {
-            for(var j in players) {//gets the data from players as to render
+        for(let i in data.updatePack) {
+            for(let j in players) {//gets the data from players as to render
                 if(players[j].id === data.updatePack[i].id) {
                     players[j].x = data.updatePack[i].x;
                     players[j].y = data.updatePack[i].y;
                     players[j].xp = data.updatePack[i].xp;
+                    players[j].targetXp = data.updatePack[i].targetXp;
                     players[j].upgrade = data.updatePack[i].upgrade;
                     players[j].size = data.updatePack[i].size;
                     players[j].doMovement = data.updatePack[i].doMovement;
@@ -81,25 +87,27 @@ function setup() {
                     players[j].angle = data.updatePack[i].angle;
                     players[j].bodyAngle = data.updatePack[i].bodyAngle;
                     players[j].doingAbility = data.updatePack[i].doingAbility;
-                    players[j].ability = data.updatePack[i].ability;
+                    players[j].whatAbility = data.updatePack[i].whatAbility;
+                    players[j].abilitySet = data.updatePack[i].abilitySet;
                 }
             }
         }
     });
 
     socket.on("plantInitPack", function(data) {
-        for(var i in data.plantInitPack) {
-            var plant = new Plant(data.plantInitPack[i].id, data.plantInitPack[i].x, data.plantInitPack[i].height,data.plantInitPack[i].hasFlower);
+        for(let i in data.plantInitPack) {
+            var plant = new Plant(data.plantInitPack[i].id, data.plantInitPack[i].x, data.plantInitPack[i].height,data.plantInitPack[i].hasFlower, data.plantInitPack[i].hasLeaf);
             plants.push(plant);
             console.log("plant init");
         }
     });
 
     socket.on("plantUpdatePack", function(data) {
-        for(var i in data.plantUpdatePack) {
-            for(var j in plants) {//gets the data from players as to render
+        for(let i in data.plantUpdatePack) {
+            for(let j in plants) {
             	if(plants[j].id === data.plantUpdatePack[i].id) {
             		plants[j].hasFlower = data.plantUpdatePack[i].hasFlower;
+            		plants[j].hasLeaf = data.plantUpdatePack[i].hasLeaf;
             		// console.log("updated flower state as "+plants[j].hasFlower)
             	}
             }
@@ -107,7 +115,7 @@ function setup() {
     });
 
     socket.on("someoneLeft", function(data) {
-        for(var i in players) {
+        for(let i in players) {
             if(players[i].id === data.id) {
                 players.splice(i, 1);
             }
@@ -117,65 +125,60 @@ function setup() {
 	createCanvas(windowWidth,windowHeight);
 	imageMode(CENTER);
 	rectMode(CORNER);
+	textAlign(CENTER, CENTER);
 }
 
 //called every frame
 function draw(){
-	background(0, 0, 200); // it gets a hex/rgb color
+	background(0, 0, 250); // it gets a hex/rgb color
     sendInputData();
-    for(var i in players) {
+    push();
+    for(let i in players) {
         if(players[i].id === myId) {
         	var adjustedX;
-        	//distance from right wall of map to right edge of screen
-        	var widthFromMapToScreen;
         	if(players[i].isFlipped){
         		adjustedX = ((width/2+players[i].size/3) - players[i].x);
-        		widthFromMapToScreen = (players[i].x-players[i].size/3+width/2)-mapSize;
         	} else{
         		adjustedX = ((width/2-players[i].size/3) - players[i].x);
-        		widthFromMapToScreen = (players[i].x+players[i].size/3+width/2)-mapSize;
         	}
     		var adjustedY = ((height*0.75+players[i].size/3.5) - players[i].y);
             translate(adjustedX, adjustedY); //zooming in to 1/3 up the player
-
-            var percentage = players[i].xp/(players[i].upgrade*100);
         }
     }
 
-    fill(0, 0, 250);
-    rect(-adjustedX, 0-adjustedY, adjustedX, windowHeight);
-    rect(mapSize, 0-adjustedY, widthFromMapToScreen, windowHeight);
+    fill(0, 0, 200);
+    rect(0, 0-adjustedY, mapSize, windowHeight);
     fill(0, 150, 0);
     rect(0, 0, mapSize, windowHeight-adjustedY);
 
-    for(var i in plants) {
+    for(let i in plants) {
         plants[i].draw();
     }
-    for(var i in plants) {
-    	for(var j in plants[i].leaves){
+    for(let i in plants) {
+    	for(let j in plants[i].leaves){
 			if(plants[i].hasLeaf[j]){
 				plants[i].leaves[j].draw();
 			}
 		}
 	}
-    for(var i in plants) {
+    for(let i in plants) {
     	if(plants[i].hasFlower){
 			plants[i].flower.draw();
 		}
     }
-
-    for(var i in players) {
+    for(let i in players) {
         players[i].draw();
     }
 
-
     pop();
-    translate(0, 0);
-    fill(0, 100, 0);
-    rect(windowWidth*0.05, windowHeight*0.85, windowWidth*0.2, windowHeight*0.05)
-    fill(0, 250, 0);
-    rect(windowWidth*0.05, windowHeight*0.85, windowWidth*0.2*percentage, windowHeight*0.05)
-    push();
+
+    for(let i in players) {
+        if(players[i].id === myId) {
+        	players[i].drawUI();
+        }
+    }
+    fill('rgba(0,0,0, 0.8)');
+    rect(windowHeight*0.02, windowHeight*0.02, windowHeight*0.25, windowHeight*0.3, 20);
 }
 
 function windowResized() {
@@ -189,18 +192,56 @@ var Player = function(id, name, x, y, size){
 	this.y = y;
 	this.size = size;
 	this.xp = 0;
+	this.targetXp = 10;
 	this.upgrade = 1;
 	this.isFlipped = false;
 	this.angle = 0;
 
 	this.doingAbility = true;
-	this.ability = "Roll";
+	this.abilitySet = [];
+	this.whatAbility;
 	this.bodyAngle = 0;
 
 	this.doMovement = true;
 	this.frontLegUp = true;
 	this.legOffsetX = 0;
 	this.shellType = "Box";
+
+	this.drawUI = function(){
+		var adjustedY = ((height*0.75+this.size/3.5) - this.y);
+		var percentage = this.xp/this.targetXp;
+		
+		fill(0, 100, 0);
+    	rect(windowWidth * 0.05, windowHeight*0.85, windowWidth*0.2, windowHeight*0.05, 20)
+    	fill(0, 250, 0);
+    	rect(windowWidth * 0.05, windowHeight*0.85, windowWidth*0.2*percentage, windowHeight*0.05, 20)
+
+    	for (let i = this.abilitySet.length - 1; i >= 0; i--) {
+    		fill(0, 50, 0);
+    		rect(windowWidth*0.95-(i*windowWidth/15+i*windowWidth/225)-windowWidth/15, windowHeight*0.85+windowHeight*0.025-windowWidth/30,windowWidth/15,windowWidth/15, 5);
+    		var tileImg;
+    		switch(this.abilitySet[i]){
+    		case "Hide":
+    			tileImg = HideUI;
+    			break;
+    		case "BoxRoll":
+    			tileImg = BoxRollUI;
+    			break;
+    		case "DomeRoll":
+    			tileImg = DomeRollUI;
+    			break;
+    		case "SpikeRoll":
+    			tileImg = SpikeRollUI;
+    			break;
+    		default:
+    			console.Log("ability image not found");
+    			img = flowerWhite;
+    			break;
+    		}
+    		image(tileImg, windowWidth*0.95-(i*windowWidth/15+i*windowWidth/225)-windowWidth/15+windowWidth/(15*2), //x
+              windowHeight*0.85+windowHeight*0.025-windowWidth/30+windowWidth/(15*2),windowWidth/15,windowWidth/15); //y, width height
+    	}
+	}
 
 	this.draw = function(){
 		var shellImg;
@@ -210,6 +251,9 @@ var Player = function(id, name, x, y, size){
 			break;
 		case "Dome":
 			shellImg = shellDome;
+			break;
+		case "Spike":
+			shellImg = shellSpike;
 			break;
 		default:
 			console.log("shell not found");
@@ -223,7 +267,7 @@ var Player = function(id, name, x, y, size){
 		} else{
 			scale(-1, 1)
 		}
-		if(!(this.doingAbility && (this.ability === "Roll" || this.ability === "Hide"))){
+		if(!(this.doingAbility && ((this.whatAbility === "BoxRoll" || this.whatAbility === "DomeRoll" || this.whatAbility === "SpikeRoll") || this.whatAbility === "Hide"))){
 			if(this.doMovement){
 				if(this.frontLegUp){
 					image(turtleFoot, this.size/4 +this.legOffsetX, -(this.size/5.5), this.size/3, this.size/3); //front (1)
@@ -249,14 +293,21 @@ var Player = function(id, name, x, y, size){
 		push();
 		translate(0, -(this.size/2)-(this.size/6));
 		if(this.doingAbility){
-			if(this.ability === "Roll"){
+			if(this.whatAbility === "BoxRoll" || this.whatAbility === "DomeRoll" || this.whatAbility === "SpikeRoll"){
 				translate(0,0+(this.size*0.35));
 				rotate(this.bodyAngle);
-			} else if(this.ability === "Hide"){
+			} else if(this.whatAbility === "Hide"){
 				translate(0,0+(this.size/6));
 			}
 		}
 		image(shellImg, 0, 0, this.size, this.size);
+		// if(!(this.isFlipped)){
+		// 	scale(-1, 1)
+		// } else{
+		// 	scale(1, 1)
+		// }
+		// scale(-1, 1)
+		// text(this.name, 0, 0, 100, 100);
 		pop();
 		pop();
 
@@ -268,8 +319,14 @@ function keyPressed() {
 	var sizeChange = 0;
 	var speedChange = 0;
 	var setAbility = false;//we need to tell the server
-	var ability;
-	if(key === "w"){
+	var abilitySet;
+	for(let i in players) {
+        if(players[i].id === myId) {
+        	abilitySet = players[i].abilitySet;
+        }
+    }
+	var whatAbility;
+	if(key === "w"){ //testing
 		sizeChange = 10;
 		console.log("bigger");
 	}
@@ -283,22 +340,34 @@ function keyPressed() {
 	if(key === "e"){
 		speedChange = 1
 	}
-	if(key === "1"){
+	if(key === "1"){ //real
 		setAbility = true;
-		ability = "Hide";
+		whatAbility = abilitySet[0];
 	}
 	if(key === "2"){
 		setAbility = true;
-		ability = "Roll";
+		whatAbility = abilitySet[1];
 	}
-	socket.emit("commandData", {sizeChange, speedChange, setAbility, ability});
+	if(key === "3"){
+		setAbility = true;
+		whatAbility = abilitySet[2];
+	}
+	if(key === "4"){
+		setAbility = true;
+		whatAbility = abilitySet[3];
+	}
+	if(key === "5"){
+		setAbility = true;
+		whatAbility = abilitySet[4];
+	}
+	socket.emit("commandData", {sizeChange, speedChange, setAbility, whatAbility});
 
 }
 
 function sendInputData() { //client specific p5 stuff that the server cant get
 	var angle;
 
-	for(var i in players) {
+	for(let i in players) {
         if(players[i].id === myId) {
     		angle = atan2(mouseY-(height*0.75+players[i].size*-0.1), mouseX-(width/2)); //angle from head level
     	}
@@ -306,7 +375,7 @@ function sendInputData() { //client specific p5 stuff that the server cant get
 
 	var distXToMouse = Math.abs(mouseX-(width/2));
 
-	var isFlipped;
+	let isFlipped;
 	if(mouseX>(width/2)){
 		isFlipped = false;
 	} else{
@@ -318,13 +387,13 @@ function sendInputData() { //client specific p5 stuff that the server cant get
 }
 
 
-var Plant = function(id, x, height, hasFlower){
+var Plant = function(id, x, height, hasFlower, hasLeaf){
 	this.id = id;
 	this.x = x;
 	this.height = height;
 	this.hasFlower = hasFlower;
 	this.flower = new Flower(x, -height);
-	this.hasLeaf = [];
+	this.hasLeaf = hasLeaf; //important
 	this.leaves = [];
 	var numLeaves = (height-(height%75))/75;
 	var doLeafFlip;
@@ -333,9 +402,12 @@ var Plant = function(id, x, height, hasFlower){
 	} else{
 		doLeafFlip = true;
 	}
-	for(var i = 0; i<numLeaves; i++){
-		this.leaves[i]=new Leaf(x, -((i+0.5)*75), doLeafFlip);
-		this.hasLeaf[i] = true;
+	for(let i = 0; i<numLeaves; i++){ //the client creates leaf placements
+		if(!(doLeafFlip)){
+			this.leaves[i]=new Leaf(x+50, -((i+0.5)*75), doLeafFlip);
+		} else{
+			this.leaves[i]=new Leaf(x-50, -((i+0.5)*75), doLeafFlip);
+		}
 		doLeafFlip = !doLeafFlip;
 	}
 	this.draw = function(){
@@ -370,11 +442,11 @@ var Leaf = function(x, y, isFlipped){
 		translate(this.x, this.y);
 		if(!(this.isFlipped)){
 			scale(1, 1)
-			image(leaf, 50, 0, this.size, this.size*3/4);
+			image(leaf, 0, 0, this.size, this.size*3/4);
 
 		} else{
 			scale(-1, 1)
-			image(leaf, 50, 0, this.size, this.size*3/4);
+			image(leaf, 0, 0, this.size, this.size*3/4);
 		}
 		pop();
 	}
